3041a93df04140524dc829e2938bec9e83b9f7ba
Gabriel Guilherme <gabriel.guilherme@compasso.com.br>
2024-05-07 17:52:41 -0300

    feat: create entity classes

--- ms-payment/src/main/java/br/shop/style/mspayment/entity/Installment.java
+++ ms-payment/src/main/java/br/shop/style/mspayment/entity/Installment.java
@@ -1,4 +1,24 @@
 package br.shop.style.mspayment.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "installments")
 public class Installment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer amount;
+
+    private String brand;
+
+    @OneToMany
+    @JoinColumn(name = "payment_id")
+    private List<Payment> payments;
 }
--- ms-payment/src/main/java/br/shop/style/mspayment/entity/Payment.java
+++ ms-payment/src/main/java/br/shop/style/mspayment/entity/Payment.java
@@ -1,4 +1,20 @@
 package br.shop.style.mspayment.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "payments")
 public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String type;
+
+    private Boolean installments;
+
+    private Boolean active;
 }
