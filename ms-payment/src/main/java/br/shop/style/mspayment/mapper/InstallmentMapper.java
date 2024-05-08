4bc5566690b5e94d4c03fd0a9f13354fcbe7edf5
Gabriel Guilherme <gabriel.guilherme@compasso.com.br>
2024-05-08 16:47:43 -0300

    feat: add MapStruct and Hibernate Validator for mapping and validation

---
+++ ms-payment/src/main/java/br/shop/style/mspayment/mapper/InstallmentMapper.java
@@ -0,0 +1,21 @@
+package br.shop.style.mspayment.mapper;
+
+import br.shop.style.mspayment.dto.request.InstallmentRequestDto;
+import br.shop.style.mspayment.dto.response.InstallmentResponseDto;
+import br.shop.style.mspayment.entity.Installment;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface InstallmentMapper {
+
+    @Mapping(source = "payment.id", target = "paymentId")
+    InstallmentResponseDto installmentToInstallmentResponseDto(Installment payment);
+
+    Installment installmentResponseDtoToInstallment(InstallmentResponseDto paymentResponseDto);
+
+    @Mapping(source = "payment.id", target = "paymentId")
+    InstallmentRequestDto installmentToInstallmentRequestDto(Installment payment);
+
+    Installment installmentRequestDtoToInstallment(InstallmentRequestDto paymentResponseDto);
+}
---
+++ ms-payment/src/main/java/br/shop/style/mspayment/mapper/PaymentMapper.java
@@ -0,0 +1,17 @@
+package br.shop.style.mspayment.mapper;
+
+import br.shop.style.mspayment.dto.request.PaymentRequestDto;
+import br.shop.style.mspayment.dto.response.PaymentResponseDto;
+import br.shop.style.mspayment.entity.Payment;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface PaymentMapper {
+    PaymentResponseDto paymentToPaymentResponseDto(Payment payment);
+
+    Payment paymentResponseDtoToPayment(PaymentResponseDto paymentResponseDto);
+
+    PaymentRequestDto paymentToPaymentRequestDto(Payment payment);
+
+    Payment paymentRequestDtoToPayment(PaymentRequestDto paymentResponseDto);
+}
