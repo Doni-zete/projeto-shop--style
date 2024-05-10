80062cb78ef1288673823362cc2d53a67b8e8b33
Gabriel Guilherme <gabriel.guilherme@compasso.com.br>
2024-05-10 13:24:44 -0300

    feat: Add custom exceptions

--- ms-payment/src/main/java/br/shop/style/mspayment/exception/PaymentMethodAlreadyExistsException.java
+++ ms-payment/src/main/java/br/shop/style/mspayment/exception/PaymentMethodAlreadyExistsException.java
@@ -1,6 +1,7 @@
 package br.shop.style.mspayment.exception;
 
 public class PaymentMethodAlreadyExistsException extends RuntimeException {
+
     public PaymentMethodAlreadyExistsException(String message) {
         super(message);
     }
